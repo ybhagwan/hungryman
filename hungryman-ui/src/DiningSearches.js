1c3452aa8ebc3a6d1c18f4646c115547920ea2bd
ybhagwan <99209398+ybhagwan@users.noreply.github.com>
2022-11-16 00:06:27 +0530

    Update DiningSearches.js

--- hungryman-ui/src/DiningSearches.js
+++ hungryman-ui/src/DiningSearches.js
@@ -16,7 +16,7 @@ function DiningSearches(props)
         <div name="diningSearches" id="diningSearches">
           <div className="bannerHeader"> 
             <div className="bannerCenter">
-            <label className="searchesHeader">Submitted Searches And Results</label>
+            <label className="searchesHeader">Submitted Searches And Results by ybhagwan</label>
             </div>      	
           </div>           
           {searches}
